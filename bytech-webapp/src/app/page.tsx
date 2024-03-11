cafae7969baf19da4d4b856ec46ab8d0d8b55a33
Israel Valderrama <48647047+isravg22@users.noreply.github.com>
2024-03-11 21:10:09 +0100

    Login Page
    Diseño pagina de login

--- bytech-webapp/src/app/page.tsx
+++ bytech-webapp/src/app/page.tsx
@@ -1,13 +1,29 @@
 import imgLogo from '@/app/img/logo.png';
 import Image from 'next/image';
+import Link from 'next/link';
+
 export default function LoginPage() {
 
+
   return (
-    <>
-        <form>
-          <h3 style={{color:'white',alignContent:'center'}}>Bienvenido a ByTech</h3>
-          <Image src={imgLogo} alt='logo ByTech'/>
+    <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
+      <div style={{backgroundColor: 'white',padding: '2em', borderRadius: '20px',width: '30%'}}>
+        <Image src={imgLogo} alt="Logo" width="256" height="256" style={{display: 'flex' ,justifyContent: 'center' ,margin:' 0 auto'}} />
+        <p style={{marginBottom: '1em',fontSize:'32px',fontWeight:'bold',textAlign:'center'}}>Bienvenido a ByTech</p>
+        <form style={{display: 'flex' ,flexDirection: 'column' ,gap: '1em'}}>
+          <label style={{fontWeight:'bold',fontSize:'20px'}}>Nombre de usuario:</label>
+          <input type="text" placeholder="Ejemplo123" style={{padding:' 0.5em',borderRadius: '5px',fontSize:'18px' }}/>
+          <label style={{fontWeight:'bold',fontSize:'20px'}}>Contraseña:</label>
+          <input type="password" placeholder="***********" style={{padding: '0.5em', borderRadius: '5px',fontSize:'18px'}} />
+          <p>
+            Si no tienes cuenta, pulsa
+            <strong><Link href="/Register"> aquí</Link></strong>
+          </p>
+          <button style={{backgroundColor: '#00a2ff', padding: '0.5em 0', borderRadius: '5px' ,border: 'none', color: 'white', fontWeight: 'bold'}}>Iniciar Sesión</button>
         </form>
-    </>
-  );
+      </div>
+    </div>
+
+
+  )
 }
--- bytech-webapp/tailwind.config.ts
+++ bytech-webapp/tailwind.config.ts
@@ -5,6 +5,9 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/flowbite-react/lib/**/*.js",
+    "./pages/**/*.{ts,tsx}",
+    "./public/**/*.html",
   ],
   theme: {
     extend: {
@@ -17,6 +20,7 @@ const config: Config = {
   },
   plugins: [
     require('@tailwindcss/forms'),
+    require('flowbite/plugin')
   ],
 };
 export default config;
