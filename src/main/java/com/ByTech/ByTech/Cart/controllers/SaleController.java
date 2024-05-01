90c5b1b8dba5a2bfcada940fe4a7f02d60c37e03
Israel Valderrama <48647047+isravg22@users.noreply.github.com>
2024-05-01 11:31:08 +0200

    SaleController ya agregado

---
+++ src/main/java/com/ByTech/ByTech/Cart/controllers/SaleController.java
@@ -0,0 +1,52 @@
+package com.ByTech.ByTech.Cart.controllers;
+
+import com.ByTech.ByTech.Cart.models.Sale;
+import com.ByTech.ByTech.Cart.services.SaleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.ByTech.ByTech.Cart.models.Message;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/sale")
+public class SaleController {
+
+    private final SaleService saleService;
+
+    @Autowired
+    public SaleController(SaleService saleService) {
+        this.saleService = saleService;
+    }
+    @GetMapping("/client")
+    public ResponseEntity<List<Sale>> getByClient() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.getPrincipal() instanceof UserDetails) {
+            String userName = ((UserDetails) authentication.getPrincipal()).getUsername();
+            return new ResponseEntity<>(this.saleService.getSalesByClient(userName), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+    }
+
+    @PostMapping(path = "/create")
+    public ResponseEntity<Message> createSale() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.getPrincipal() instanceof UserDetails) {
+            String userName = ((UserDetails) authentication.getPrincipal()).getUsername();
+            this.saleService.createSale(userName);
+            return new ResponseEntity<>(new Message("Compra exitosa"), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+    }
+}
