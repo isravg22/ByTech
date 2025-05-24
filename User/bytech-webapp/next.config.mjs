6881a16ad40678172225585fb105a47c9618b223
Israel Valderrama <isvalgar@gmail.com>
2025-05-24 02:48:11 +0200

    Paquetes Backend

--- User/bytech-webapp/next.config.mjs
+++ User/bytech-webapp/next.config.mjs
@@ -1,9 +1,8 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['s3-bytech.s3.eu-west-3.amazonaws.com'],
-    },
+  images: {
+    domains: ['s3-bytech.s3.eu-west-3.amazonaws.com', 'drive.google.com'],
+  },
 };
 
 export default nextConfig;
--- User/bytech-webapp/src/app/Home/page.tsx
+++ User/bytech-webapp/src/app/Home/page.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import NavBar from '@/Component/NavBar/Navbar';
 import Footer from '@/Component/Footer/Footer';
 import { Producto } from '@/Interface/Product';
+import Image from 'next/image';
 
 const HomePage = () => {
   const [products, setProducts] = useState<Producto[]>([]);
@@ -49,7 +50,7 @@ const HomePage = () => {
         {products.map((product) => (
           <div key={product.id} style={{ flex: '0 1 23%', padding: '20px', marginBottom: '40px', border: '1px solid #ccc', borderRadius: '8px', backgroundColor: '#fff', textAlign: 'center', marginLeft: '1%', marginRight: '1%' }}>
             <div style={{ marginBottom: '20px', width: '100%', height: '200px', display: 'flex', justifyContent: 'center', alignItems: 'center', overflow: 'hidden' }}>
-              <img src={product.image} alt={product.name} style={{ width: '40%', height: '100%' }} />
+              <Image src={product.image} alt={product.name} style={{ width: '40%', height: '100%' }} width={500} height={500}/>
             </div>
             <div style={{ textAlign: 'left' }}>
               <h2 style={{ margin: '0 0 10px 0', fontSize: '1.2rem', fontWeight: 'bold' }}>{product.name.toUpperCase()}</h2>
